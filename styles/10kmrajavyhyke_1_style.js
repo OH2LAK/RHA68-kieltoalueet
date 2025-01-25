9c6466d215a33b97a4c989b756b878a14f2f15ec
Erik Finskas <erik.finskas@gmail.com>
2025-01-25 14:09:20 +0200

    Initial commit

---
+++ styles/10kmrajavyhyke_1_style.js
@@ -0,0 +1,30 @@
+var size = 0;
+var placement = 'point';
+
+var style_10kmrajavyhyke_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; var value = feature.get("");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("NAME_LEN") !== null) {
+        labelText = String(feature.get("NAME_LEN"));
+    }
+    var style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(227,26,28,0.25)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
